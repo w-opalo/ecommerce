3ffe35e47fe4e7bdca1f03781647eb64c1b4fd86
Willice Opalo <williceopalo11@gmail.com>
2025-03-21 23:47:03 +0300

    the welcome page modified

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -1,4 +1,5 @@
-import AuthLayoutTemplate from '@/layouts/auth/auth-split-layout';
+import AuthLayoutTemplate from '@/layouts/auth/auth-simple-layout';
+// import navbar from '@/components/ui/Navbar';
 
 export default function AuthLayout({ children, title, description, ...props }: { children: React.ReactNode; title: string; description: string }) {
     return (
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,19 +1,22 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import Navbar from '@/components/ui/navbar';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboardss',
+        title: 'Dashboards',
         href: '/dashboard',
     },
-];
+]; 
 
 export default function Dashboard() {
     return (
+        <> 
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
+            <Navbar />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
@@ -31,5 +34,6 @@ export default function Dashboard() {
                 </div>
             </div>
         </AppLayout>
+        </>
     );
 }
