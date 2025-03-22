afe8397192e168dcf50a4744720a7948da1d22fc
Willice Opalo <williceopalo11@gmail.com>
2025-03-22 19:45:11 +0300

    permissions

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -19,7 +19,7 @@ export default function Welcome() {
                     <div className="max-w-md">
                     <h1 className="text-5xl font-bold">Hello there</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        ppProvident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
                     <button className="btn btn-primary">Get Started</button>
