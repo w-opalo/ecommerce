af58d4914cebd8a254698d1dc53547e57c4d53b9
Willice Opalo <williceopalo11@gmail.com>
2025-03-25 00:51:40 +0300

    products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
